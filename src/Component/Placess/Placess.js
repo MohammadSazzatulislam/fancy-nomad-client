5aa935ed4b506295444b22350cde2795458a3a54
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-12-25 12:51:46 +0600

    add placess section

---
+++ src/Component/Placess/Placess.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { FaLocationArrow } from "react-icons/fa";
+
+const Placess = () => {
+  return (
+    <div className="my-28 px-5">
+      <h1 className="lg:text-5xl text-center  md:text-4xl text-3xl font-bold uppercase leading-8 ">
+        Travel Most Popular Place In <br /> Bangladesh
+      </h1>
+      <div className="flex flex-wrap justify-center items-center gap-5 mt-28">
+        {/* <!--Card 1--> */}
+        <div className="w-full lg:w-96 md:w-80 border  rounded overflow-hidden shadow-lg">
+          <img
+            className="w-full"
+            src="https://images.unsplash.com/photo-1590603740183-980e7f6920eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y294cyUyMGJhemFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4 flex gap-2 justify-center items-center">
+            <FaLocationArrow className="w-5 h-5"></FaLocationArrow>
+            <p className="font-bold text-xl mb-2">Cox's Buzzer ,</p>
+            <p className="font-bold text-xl mb-2">Chattogram</p>
+          </div>
+        </div>
+        {/* <!--Card 1--> */}
+        {/* <!--Card 1--> */}
+        <div className="w-full lg:w-96 md:w-80 border  rounded overflow-hidden shadow-lg">
+          <img
+            className="w-full"
+            src="https://images.unsplash.com/photo-1590603740183-980e7f6920eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y294cyUyMGJhemFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4 flex gap-2 justify-center items-center">
+            <FaLocationArrow className="w-5 h-5"></FaLocationArrow>
+            <p className="font-bold text-xl mb-2">Cox's Buzzer ,</p>
+            <p className="font-bold text-xl mb-2">Chattogram</p>
+          </div>
+        </div>
+        {/* <!--Card 1--> */}
+        {/* <!--Card 1--> */}
+        <div className="w-full lg:w-96 md:w-80 border  rounded overflow-hidden shadow-lg">
+          <img
+            className="w-full"
+            src="https://images.unsplash.com/photo-1590603740183-980e7f6920eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y294cyUyMGJhemFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4 flex gap-2 justify-center items-center">
+            <FaLocationArrow className="w-5 h-5"></FaLocationArrow>
+            <p className="font-bold text-xl mb-2">Cox's Buzzer ,</p>
+            <p className="font-bold text-xl mb-2">Chattogram</p>
+          </div>
+        </div>
+        {/* <!--Card 1--> */}
+        {/* <!--Card 1--> */}
+        <div className="w-full lg:w-96 md:w-80 border  rounded overflow-hidden shadow-lg">
+          <img
+            className="w-full"
+            src="https://images.unsplash.com/photo-1590603740183-980e7f6920eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y294cyUyMGJhemFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4 flex gap-2 justify-center items-center">
+            <FaLocationArrow className="w-5 h-5"></FaLocationArrow>
+            <p className="font-bold text-xl mb-2">Cox's Buzzer ,</p>
+            <p className="font-bold text-xl mb-2">Chattogram</p>
+          </div>
+        </div>
+        {/* <!--Card 1--> */}
+        {/* <!--Card 1--> */}
+        <div className="w-full lg:w-96 md:w-80 border  rounded overflow-hidden shadow-lg">
+          <img
+            className="w-full"
+            src="https://images.unsplash.com/photo-1590603740183-980e7f6920eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y294cyUyMGJhemFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4 flex gap-2 justify-center items-center">
+            <FaLocationArrow className="w-5 h-5"></FaLocationArrow>
+            <p className="font-bold text-xl mb-2">Cox's Buzzer ,</p>
+            <p className="font-bold text-xl mb-2">Chattogram</p>
+          </div>
+        </div>
+        {/* <!--Card 1--> */}
+        {/* <!--Card 1--> */}
+        <div className="w-full lg:w-96 md:w-80 border  rounded overflow-hidden shadow-lg">
+          <img
+            className="w-full"
+            src="https://images.unsplash.com/photo-1590603740183-980e7f6920eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y294cyUyMGJhemFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4 flex gap-2 justify-center items-center">
+            <FaLocationArrow className="w-5 h-5"></FaLocationArrow>
+            <p className="font-bold text-xl mb-2">Cox's Buzzer ,</p>
+            <p className="font-bold text-xl mb-2">Chattogram</p>
+          </div>
+        </div>
+        {/* <!--Card 1--> */}
+        {/* <!--Card 1--> */}
+        <div className="w-full lg:w-96 md:w-80 border  rounded overflow-hidden shadow-lg">
+          <img
+            className="w-full"
+            src="https://images.unsplash.com/photo-1590603740183-980e7f6920eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y294cyUyMGJhemFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4 flex gap-2 justify-center items-center">
+            <FaLocationArrow className="w-5 h-5"></FaLocationArrow>
+            <p className="font-bold text-xl mb-2">Cox's Buzzer ,</p>
+            <p className="font-bold text-xl mb-2">Chattogram</p>
+          </div>
+        </div>
+        {/* <!--Card 1--> */}
+      </div>
+    </div>
+  );
+};
+
+export default Placess;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nature from '../../Component/Nature/Nature';
 import News from '../../Component/News/News';
+import Placess from '../../Component/Placess/Placess';
 
 const Home = () => {
     return (
         <div>
             <News></News>
             <Nature></Nature>
+            <Placess></Placess>
         </div>
     );
 };
