fb8b781032d8f8f62f083f3d2f99c221edbac890
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-12-25 19:28:55 +0600

    add react aos

--- src/Component/Nature/Nature.js
+++ src/Component/Nature/Nature.js
@@ -6,13 +6,34 @@ const Nature = () => {
     <div className="my-28 px-5">
       <ParticleBacground></ParticleBacground>
       <div className=" text-center flex justify-center items-center flex-col">
-        <h1 className="lg:text-6xl  md:text-4xl text-3xl font-semibold uppercase leading-8 ">
-          LET'S ENJOY THE WONDERS OF THE NATURE TOGETHER!
-        </h1>
-        <p className="text-md mt-3">
+        <div className="lg:text-6xl  md:text-4xl text-3xl font-semibold uppercase leading-8 ">
+          <h1
+            className="lg:text-6xl  md:text-4xl text-3xl font-semibold uppercase leading-8 "
+            data-aos="fade-down"
+            data-aos-easing="linear"
+            data-aos-duration="500"
+          >
+            LET'S ENJOY THE WONDERS OF THE
+          </h1>
+          <h2
+            className="lg:text-6xl  md:text-4xl text-3xl font-semibold uppercase leading-8 "
+            data-aos="fade-up"
+            data-aos-easing="linear"
+            data-aos-duration="500"
+          >
+            NATURE TOGETHER!
+          </h2>
+        </div>
+        <p
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-delay="500"
+          data-aos-offset="0"
+          className="text-md mt-3 text-center"
+        >
           At our travel agency we offer a variety of tours and destinations to
-          all kinds of travelers. Here is what you can get at Travel Guide,
-          regardless of your traveling needs and desires.
+          all kinds of travelers.Here is what you can get at Travel Guide, <br /> regardless of
+          your traveling needs and desires.
         </p>
       </div>
       <div className="mt-28 flex flex-wrap gap-5 justify-center items-center">
@@ -24,8 +45,22 @@ const Nature = () => {
             alt="Mountain"
           />
           <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Mountain</div>
-            <p className="text-gray-700 text-base">
+            <p
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              className="font-bold text-xl mb-2"
+            >
+              Mountain
+            </p>
+            <p
+              data-aos="fade-left"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              className="text-gray-700 text-base"
+            >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, Nonea! Maiores et perferendis eaque,
               exercitationem praesentium nihil.
@@ -36,8 +71,22 @@ const Nature = () => {
         {/* <!--Card 2--> */}
         <div className="w-full lg:w-96 md:w-80 border rounded overflow-hidden shadow-lg">
           <div className="px-6 py-4 ">
-            <div className="font-bold text-xl mb-2">Mountain</div>
-            <p className="text-gray-700 text-base">
+            <p
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              className="font-bold text-xl mb-2"
+            >
+              Mountain
+            </p>
+            <p
+              data-aos="fade-left"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              className="text-gray-700 text-base"
+            >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, Nonea! Maiores et perferendis eaque,
               exercitationem praesentium nihil.
@@ -58,8 +107,22 @@ const Nature = () => {
             alt="Mountain"
           />
           <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Mountain</div>
-            <p className="text-gray-700 text-base">
+            <p
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              className="font-bold text-xl mb-2"
+            >
+              Mountain
+            </p>
+            <p
+              data-aos="fade-left"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              className="text-gray-700 text-base"
+            >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, Nonea! Maiores et perferendis eaque,
               exercitationem praesentium nihil.
