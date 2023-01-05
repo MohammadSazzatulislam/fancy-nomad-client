6624b4901b32f5e54d8c29b169b6678e54a8b6fb
MohammadSazzatulIslam <rockstarwds@gmail.com>
2023-01-05 16:44:51 +0600

    update login and sign up form

--- src/Shared/LogIn/LogIn.js
+++ src/Shared/LogIn/LogIn.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
 import { UserAuthContext } from "../../Context/AuthContext/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate ,Link } from "react-router-dom";
+import './Login.css';
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const LogIn = () => {
   const [userInfo, setUserInfo] = useState({
@@ -44,11 +46,11 @@ const LogIn = () => {
     <div>
       <div className="relative">
         <img
-          src="https://images.pexels.com/photos/3228766/pexels-photo-3228766.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          src="https://wallpaperset.com/w/full/2/b/0/221375.jpg"
           className="absolute inset-0 object-cover w-full h-full "
           alt=""
         />
-        <div className="relative bg-opacity-75 bg-deep-purple-accent-700">
+        <div className="relative opacity-100 bg-deep-purple-accent-900">
           <svg
             className="absolute inset-x-0 bottom-0 text-white"
             viewBox="0 0 1160 163"
@@ -58,99 +60,57 @@ const LogIn = () => {
               d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
             />
           </svg>
-          <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-            <div className="flex flex-col items-center justify-between xl:flex-row">
-              <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                  The quick, brown fox <br className="hidden md:block" />
-                  jumps over a lazy dog
-                </h2>
-                <p className="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudan, totam rem aperiam, eaque ipsa
-                  quae.
-                </p>
-                <a
-                  href="/"
-                  aria-label=""
-                  className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-accent-400 hover:text-teal-accent-700"
+          <div className=" flex p-5 w-full justify-center lg:justify-end items-centee">
+            <div className=" lg:w-[450px] loginform outline-none relative mx-auto overflow-hidden w-full border-none text-white rounded shadow-5xl p-5 ">
+              <h3 className="lg:text-5xl textGradient  md:text-4xl text-3xl font-bold uppercase leading-8 my-3 text-center ">
+                Log In
+              </h3>
+              <form>
+                <div className=" my-7 flex-grow relative inputBox">
+                  <input
+                    className="bg-transparent text-white border-none outline-none w-full shadow-none px-2 pb-2 leading-3 text-md"
+                    required
+                    type="email"
+                    name="email"
+                  />
+                  <span className="absolute uppercase pointer-events-none left-0  leading-3 text-white font-semibold ">
+                    Email
+                  </span>
+                  <i className="absolute left-0 bottom-0 w-full bg-white overflow-hidden h-1"></i>
+                </div>
+                <div className=" mb-7 flex-grow relative inputBox">
+                  <input
+                    className="bg-transparent text-white border-none outline-none w-full shadow-none px-2 pb-2 leading-3 text-md"
+                    required
+                    type="password"
+                    name="password"
+                  />
+                  <span className="absolute uppercase pointer-events-none left-0  leading-3 text-white font-semibold ">
+                    Password
+                  </span>
+                  <i className="absolute left-0 bottom-0 w-full bg-white overflow-hidden h-1"></i>
+                </div>
+                <h1 className="text-white underline text-end font-medium mb-3">
+                  Forgot Password
+                </h1>
+                <button
+                  type="submit"
+                  className=" w-full uppercase font-semibold tracking-wide py-2 bg-blue-500 hover:bg-blue-700 outline-none  rounded-sm shadow-xl "
                 >
-                  Learn more
-                  <svg
-                    className="inline-block w-3 ml-2"
-                    fill="currentColor"
-                    viewBox="0 0 12 12"
-                  >
-                    <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
-                  </svg>
-                </a>
-              </div>
-              <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
-                <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
-                  <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
-                    Sign up for updates
-                  </h3>
-                  <form>
-                    <div className="mb-1 sm:mb-2">
-                      <label
-                        htmlFor="firstName"
-                        className="inline-block mb-1 font-medium"
-                      >
-                        First name
-                      </label>
-                      <input
-                        placeholder="John"
-                        required
-                        type="text"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="firstName"
-                        name="firstName"
-                      />
-                    </div>
-                    <div className="mb-1 sm:mb-2">
-                      <label
-                        htmlFor="lastName"
-                        className="inline-block mb-1 font-medium"
-                      >
-                        Last name
-                      </label>
-                      <input
-                        placeholder="Doe"
-                        required
-                        type="text"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="lastName"
-                        name="lastName"
-                      />
-                    </div>
-                    <div className="mb-1 sm:mb-2">
-                      <label
-                        htmlFor="email"
-                        className="inline-block mb-1 font-medium"
-                      >
-                        E-mail
-                      </label>
-                      <input
-                        placeholder="john.doe@example.org"
-                        required
-                        type="text"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="email"
-                        name="email"
-                      />
-                    </div>
-                    <div className="mt-4 mb-2 sm:mb-4">
-                      <button
-                        type="submit"
-                        className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                      >
-                        Subscribe
-                      </button>
-                    </div>
-                    <p className="text-xs text-gray-600 sm:text-sm">
-                      We respect your privacy. Unsubscribe at any time.
-                    </p>
-                  </form>
+                  Log In
+                </button>
+                <div className="flex mt-3 justify-between items-center ">
+                  <h1>Don't have any account?</h1>
+                  <Link to="/signup">
+                    <p className="font-semibold underline">Sign Up</p>
+                  </Link>
+                </div>
+              </form>
+              <div className=" mt-7">
+                <div className="divider">OR</div>
+                <div className="flex justify-center items-center gap-5">
+                  <FaGoogle className="w-12 h-12"></FaGoogle>
+                  <FaGithub className="w-12 h-12"></FaGithub>
                 </div>
               </div>
             </div>
